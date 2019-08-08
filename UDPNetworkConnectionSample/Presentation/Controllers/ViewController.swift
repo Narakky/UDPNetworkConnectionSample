cc0513e90bcf7b138ec0e6dc56662c0a687931fe
Narakky <11222997+Narakky@users.noreply.github.com>
2019-08-08 18:16:55 +0900

    carthage set up

--- UDPNetworkConnectionSample/Presentation/Controllers/ViewController.swift
+++ UDPNetworkConnectionSample/Presentation/Controllers/ViewController.swift
@@ -8,13 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  // MARK: - Definitions
+
+  private enum TableData: CaseIterable {
+    case connectionTest
+    case settings
+
+    var title: String {
+      switch self {
+      case .connectionTest: return "接続テスト"
+      case .settings: return "設定"
+      }
     }
 
+    var detail: String {
+      switch self {
+      case .connectionTest: return "UDP通信の接続テストを行います"
+      case .settings: return "接続先の設定などを行います"
+      }
+    }
+  }
+
+  // MARK: - Outlet
+
+  @IBOutlet private weak var tableView: UITableView! {
+    didSet {
+      tableView.delegate = self
+      tableView.dataSource = self
+      tableView.tableFooterView = UIView()
+      tableView.separatorInset = .zero
+    }
+  }
+
+  // MARK: - Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    if let selectedIndexPath = tableView.indexPathForSelectedRow {
+      tableView.deselectRow(at: selectedIndexPath, animated: false)
+    }
+  }
 
 }
 
+// MARK: - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return TableData.allCases.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
+    let tableData = TableData.allCases[indexPath.row]
+    cell.textLabel?.text = tableData.title
+    cell.detailTextLabel?.text = tableData.detail
+    return cell
+  }
+}
+
+// MARK: - UITableViewDelegate
+
+extension ViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let tableData = TableData.allCases[indexPath.row]
+    switch tableData {
+    case .connectionTest:
+      break
+    case .settings:
+      break
+    }
+  }
+}
